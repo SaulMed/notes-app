e98ad110ddf7e865c7711a08fc28d06df4a92a4c
Saul Medina <elesedelsaul@gmail.com>
2020-06-20 14:09:42 -0500

    Final

--- src/database.js
+++ src/database.js
@@ -1,10 +1,9 @@
 const mongoose = require('mongoose')
 
-const { MONGODB_HOST, MONGODB_DATABASE} = process.env;
+const { MONGODB_URI } = process.env;
 
-const MONGODB_URI = `mongodb://${MONGODB_HOST}/${MONGODB_DATABASE}`;    
 
-mongoose.connect(MONGODB_URI,{
+mongoose.connect(`${MONGODB_URI}`,{
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex:true
