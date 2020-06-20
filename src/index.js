68468db85458689ebd8938c13d6d98365eb89448
Saul Medina <elesedelsaul@gmail.com>
2020-06-20 14:41:27 -0500

    Ultimate

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
+if(process.env.NODE_ENV = 'development'){
     //Permitir trabajar con las variables de entorno
 require('dotenv').config(); 
+}    
     //Aplicacion
 require('./server');
     //Database
 require('./database')
+
+console.log(process.env.NODE_ENV)
 
